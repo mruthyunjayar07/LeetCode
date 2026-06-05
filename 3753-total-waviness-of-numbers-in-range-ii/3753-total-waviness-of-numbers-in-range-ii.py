6c4d7addf7bb7d40593b32eee43a4a24c72d187f
Mruthyunjaya Rajendra <mruthyunjayar07@gmail.com>
2026-06-05 19:00:07 +0530

    Time: 708 ms (82.5%), Space: 68.1 MB (60%) - LeetHub

---
+++ 3753-total-waviness-of-numbers-in-range-ii/3753-total-waviness-of-numbers-in-range-ii.py
@@ -0,0 +1,61 @@
+from functools import cache
+
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+
+        def solve(n):
+            if n <= 0:
+                return 0
+
+            s = str(n)
+
+            @cache
+            def dfs(pos, tight, started, prev2, prev1):
+                if pos == len(s):
+                    return (1, 0) if started else (0, 0)
+
+                limit = int(s[pos]) if tight else 9
+
+                total_cnt = 0
+                total_wave = 0
+
+                for d in range(limit + 1):
+                    ntight = tight and d == limit
+
+                    if not started and d == 0:
+                        cnt, wave = dfs(
+                            pos + 1,
+                            ntight,
+                            False,
+                            -1,
+                            -1
+                        )
+                    else:
+                        add = 0
+
+                        if prev2 != -1:
+                            if (
+                                (prev1 > prev2 and prev1 > d)
+                                or
+                                (prev1 < prev2 and prev1 < d)
+                            ):
+                                add = 1
+
+                        cnt, wave = dfs(
+                            pos + 1,
+                            ntight,
+                            True,
+                            prev1,
+                            d
+                        )
+
+                        wave += add * cnt
+
+                    total_cnt += cnt
+                    total_wave += wave
+
+                return total_cnt, total_wave
+
+            return dfs(0, True, False, -1, -1)[1]
+
+        return solve(num2) - solve(num1 - 1)
